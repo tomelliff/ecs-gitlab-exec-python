281d579cc41189057ae166c0c81760e73ac27158
Tom Elliff <tomelliff@gmail.com>
2018-02-18 23:29:44 +0000

    Initial commit
    Mostly a brain dump and quickly hacked together, untested Python code exploring what needs to be done to create an ECS Gitlab executor.
    Once I have something crudely hacked together I'll throw this all away and rewrite in Go so that I can hook it into the Gitlab Runner stuff.

---
+++ README.md
@@ -0,0 +1,26 @@
+# ECS Gitlab Executor hacking
+
+Working out the flow of what needs to happen for there to be an ECS Gitlab executor. Easier for me to work this out in Python first and then implement it in Go.
+
+### Brain dump of stuff to do:
+
+- register runner does nothing with ecs
+- when picking up a job it will create a task definition with the runner config plus the image+tag. Command should be 'sh' (not bash?)
+	- what about services? these should be a multi container task with links?
+	- on second run it should check that task definition is the same (image+tag/services not changed) and then just use it instead of creating again (does this really save any time over removing the task definition each time?)
+		- if the task definition does need to change (eg image tag has changed) then it should be updated
+		- what happens if the runner definition changes? only gets picked up when the job runs so if a mount point is added or different ecs cluster specified then we would need to change it too
+		- maybe simpler to just throw the task definition away after every run
+- it should then run the task with an override on the env vars, passing in variables from the job
+- once the task is running it should then find the container instance it is running on and then get the ip address (use private if flag is set on runner, otherwise public) and connect to remote docker socket on container instance (runner must be able to reach the container instance on docker port and container instance must be exposing socket to 0.0.0.0/0
+- from there just run the normal docker stuff, passing in commands as the docker executor does?
+- when the job exits the task definition should remain unless the runner is being unregistered in which case it should remove all the task definitions it created
+
+### TODO:
+
+- create minimal ecs cluster allowing access to docker daemon remotely
+    - `DOCKER_OPTS="-H tcp://0.0.0.0:2376"`
+    - should look at what needs to be done for TLS communication as well (https://docs.docker.com/engine/security/https/#daemon-modes)
+- run ECS tasks and then exec into them and stream the output back out
+- handle services (linked containers in the task)
+- test if it's worth keeping task definitions around if we potentially have to check them every single job run (image/services may have changed)
---
+++ ecs.py
@@ -0,0 +1,105 @@
+import boto3
+import docker
+
+ec2 = boto3.resource('ec2')
+ecs_client = boto3.client('ecs')
+
+
+def register_task_definition():
+    task_family = 'tom-test'
+    network_mode = 'bridge'
+
+    container_name = 'tom-test'
+    image = 'alpine'
+
+    response = ecs_client.register_task_definition(
+        family=task_family,
+        networkMode=network_mode,
+        containerDefinitions=[
+            {
+                'name': container_name,
+                'image': image,
+                'memoryReservation': 128,
+                'command': [
+                    'sh',
+                ],
+                'environment': [
+                    {
+                        'name': 'foo',
+                        'value': 'bar'
+                    },
+                ]
+            },
+        ]
+    )
+
+    return response['taskDefinition']['taskDefinitionArn']
+
+
+def run_task(task_definition, cluster):
+    gitlab_ci_runner_name = 'foobar'
+
+    response = ecs_client.run_task(
+        cluster=cluster,
+        taskDefinition=task_definition,
+        overrides={
+            'containerOverrides': [
+                {
+                    'environment': [
+                        {
+                            'name': 'foo',
+                            'value': 'baz'
+                        },
+                    ]
+                }
+            ]
+        },
+        count=1,
+        startedBy=gitlab_ci_runner_name,
+    )
+
+    task = response['tasks'][0]
+    task_arn = task['taskArn']
+    container_instance_arn = task['containerInstanceArn']
+
+    return task_arn, container_instance_arn
+
+
+def get_instance_id(cluster, container_instance_arn):
+    response = ecs_client.describe_container_instances(
+        cluster=cluster,
+        containerInstances=[
+            container_instance_arn,
+        ]
+    )
+
+    return response['containerInstances'][0]['ec2InstanceId']
+
+
+def get_instance_ip(instance_id, private_ip=True):
+    instance = ec2.Instance(instance_id)
+
+    if private_ip:
+        return instance.attributes.get('private_ip_address')
+    else:
+        return instance.attributes.get('public_ip_address')
+
+
+def exec_remote_container(remote_url, command):
+    client = docker.DockerClient(base_url=remote_url, version='auto')
+    return client.containers.exec_run(command)
+
+
+def stop_task(cluster, task):
+    ecs_client.stop_task(
+        cluster=cluster,
+        task=task,
+    )
+
+
+def run_job():
+    task_definition_arn = register_task_definition()
+    task_arn, container_instance_arn = run_task(task_definition_arn, 'tom-test')
+    ip_address = get_instance_ip(get_instance_id('tom-test', container_instance_arn))
+    exec_remote_container('tcp://{}:2376'.format(ip_address), 'ls -la /')
+    stop_task('tom-test', task_arn)
